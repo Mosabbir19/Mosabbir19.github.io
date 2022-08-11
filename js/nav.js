aa29db7a114f49334c8c9c9be66ccb1a5bdda5b6
Mosabbir Bhuiyan <42299049+Mosabbir19@users.noreply.github.com>
2022-08-12 01:26:52 +0600

    Add files via upload

---
+++ js/nav.js
@@ -0,0 +1,8 @@
+navbar = document.querySelector(".navbar").querySelectorAll("a");
+
+navbar.forEach(element => {
+    element.addEventListener("click", function(){
+        navbar.forEach(nav=>nav.classList.remove("active"))
+        this.classList.add("active")
+    })
+});
